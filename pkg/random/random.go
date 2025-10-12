6e4696ea02636420223230d6f4f4a720a82ec9bc
fallrain <tozhimukhammedov.a@gmail.com>
2025-10-12 21:20:33 +0300

    переход на sync.Pool

--- pkg/random/random.go
+++ pkg/random/random.go
@@ -1,57 +1,37 @@
-// Быстрый генератор случайных чисел для многопоточного приложения
 package random
 
 import (
 	"math/rand"
-	"runtime"
+	"sync"
 	"time"
-	"unsafe"
 )
 
-var (
-	generators    []*rand.Rand
-	numGenerators int
-)
-
-func init() {
-	numGenerators = runtime.GOMAXPROCS(0) // Количество ядер
-	generators = make([]*rand.Rand, numGenerators)
-
-	// Создаем отдельный генератор для каждого ядра
-	for i := 0; i < numGenerators; i++ {
-		generators[i] = rand.New(rand.NewSource(time.Now().UnixNano() + int64(i*1000)))
-	}
-}
-
-// Получение хэша от ID горутины
-func getGoroutineID() int {
-	// Используем указатель на стек как уникальный ID горутины
-	var dummy byte
-	return int(uintptr(unsafe.Pointer(&dummy)))
-}
-
-func getRandom() *rand.Rand {
-	// Берем хэш от ID горутины и находим соответствующий генератор
-	gid := getGoroutineID()
-	idx := gid % numGenerators
-	if idx < 0 {
-		idx = -idx
-	}
-	return generators[idx]
+var randPool = sync.Pool{
+	New: func() interface{} {
+		return rand.New(rand.NewSource(time.Now().UnixNano()))
+	},
 }
 
 func FastFloat64() float64 {
-	return getRandom().Float64()
+	r := randPool.Get().(*rand.Rand)
+	defer randPool.Put(r)
+	return r.Float64()
 }
 
 func FastIntn(n int) int {
-	return getRandom().Intn(n)
+	r := randPool.Get().(*rand.Rand)
+	defer randPool.Put(r)
+	return r.Intn(n)
 }
 
 func FastUint64() uint64 {
-	return getRandom().Uint64()
+	r := randPool.Get().(*rand.Rand)
+	defer randPool.Put(r)
+	return r.Uint64()
 }
 
 func FastInt63() int64 {
-	return getRandom().Int63()
+	r := randPool.Get().(*rand.Rand)
+	defer randPool.Put(r)
+	return r.Int63()
 }

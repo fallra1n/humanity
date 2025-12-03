39846471fea4ad7b260fa732538d087baa03af0d
fallrain <tozhimukhammedov.a@gmail.com>
2025-12-04 00:53:51 +0300

    Добавлено логирование в csv

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 	"sync"
 	"time"
 
@@ -105,6 +108,69 @@ func processBirths(people []*components.Human, globalTargets []*components.Globa
 	}
 
 	return newChildren
+}
+
+// logToCSV writes the current state of all people to a CSV file
+func logToCSV(people []*components.Human, hour uint64) error {
+	filename := "log.csv"
+	
+	// Check if file exists to determine if we need to write headers
+	fileExists := false
+	if _, err := os.Stat(filename); err == nil {
+		fileExists = true
+	}
+	
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	
+	// Write header if this is the first time
+	if !fileExists {
+		header := []string{"hour", "agent_id", "age", "gender", "alive", "money", "location", "building_type", "job_status", "marital_status"}
+		if err := writer.Write(header); err != nil {
+			return err
+		}
+	}
+	
+	// Write data for each person
+	for _, person := range people {
+		location := "unknown"
+		buildingType := "unknown"
+		
+		if person.CurrentBuilding != nil {
+			location = person.CurrentBuilding.Name
+			buildingType = string(person.CurrentBuilding.Type)
+		}
+		
+		jobStatus := "unemployed"
+		if person.Job != nil {
+			jobStatus = "employed"
+		}
+		
+		row := []string{
+			strconv.FormatUint(hour, 10),
+			strconv.Itoa(components.GlobalHumanStorage.Get(person)),
+			fmt.Sprintf("%.2f", person.Age),
+			string(person.Gender),
+			fmt.Sprintf("%t", !person.Dead),
+			strconv.FormatInt(person.Money, 10),
+			location,
+			buildingType,
+			jobStatus,
+			string(person.MaritalStatus),
+		}
+		
+		if err := writer.Write(row); err != nil {
+			return err
+		}
+	}
+	
+	return nil
 }
 
 func main() {
@@ -337,9 +403,14 @@ func main() {
 				}
 			}
 		}
-
+		
+		// Log current state to CSV
+		if err := logToCSV(people, hour); err != nil {
+			log.Printf("Warning: Failed to write to log.csv: %v", err)
+		}
+		
 		iterateTimer += time.Since(startTime)
-
+		
 		// Increment global time
 		utils.GlobalTick.Increment()
 	}
